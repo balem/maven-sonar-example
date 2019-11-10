faae49ecfc59a00e340162458e4f3ef6995db9eb
Enrique Rodríguez <enriquev.rodriguez@gmail.com>
2019-11-10 16:57:21 -0300

    fisrt commit

---
+++ src/main/java/py/una/pol/mavensonarexample/MavenSonarExampleApplication.java
@@ -0,0 +1,13 @@
+package py.una.pol.mavensonarexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenSonarExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenSonarExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/py/una/pol/mavensonarexample/MavenSonarExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package py.una.pol.mavensonarexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenSonarExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
