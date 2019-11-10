42b9788c1e2975a31b5d241d0873e9ec8147361b
Enrique Rodríguez <enriquev.rodriguez@gmail.com>
2019-11-10 17:49:59 -0300

    entities de login

---
+++ src/main/java/py/una/pol/mavensonarexample/entity/User.java
@@ -0,0 +1,30 @@
+package py.una.pol.mavensonarexample.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name ="users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @Column(name = "username", unique = true, nullable = false, length = 45)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 60)
+    private String password;
+
+    @Column(name = "enabled", nullable = false)
+    private boolean enabled;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "user")
+    private Set<UserRole> userRole;
+
+}
---
+++ src/main/java/py/una/pol/mavensonarexample/entity/UserRole.java
@@ -0,0 +1,20 @@
+package py.una.pol.mavensonarexample.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_role", uniqueConstraints = @UniqueConstraint(columnNames = { "role", "username" }))
+public class UserRole {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "user_role_id", unique = true, nullable = false)
+    private Integer userRoleId;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "username", nullable = false)
+    private User user;
+
+    @Column(name = "role", nullable = false, length = 45)
+    private String role;
+}
